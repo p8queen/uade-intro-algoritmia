f237b2319754717aeaeb6ed61303d94561cd8ca9
Gustavo Daniel Carmona <p8queen@gmail.com>
2024-12-07 12:34:55 -0300

    Delete first.py

--- final-examen/first.py
+++
@@ -1,8 +0,0 @@
-import random
-
-n = int(input('ingrese un numero menor a 10: '))
-while n<1 or n>10:
-	n = int(input('ingrese un numero menor a 10: '))
-a = random.randint(1,10)
-print(n, a)
-print()
